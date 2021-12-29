de9a983540231c7677bf8f8d5af280b7258f31ec
Eduardo Rodrigues <contatoeduardofelipe@hotmail.com>
2021-12-29 17:45:43 -0300

    Correção de alguns erros

--- src/modules/games/repositories/implementations/GamesRepository.ts
+++ src/modules/games/repositories/implementations/GamesRepository.ts
@@ -15,7 +15,7 @@ export class GamesRepository implements IGamesRepository {
   async findByTitleContaining(param: string): Promise<Game[]> {
     return this.repository
       .createQueryBuilder("games")
-      .where("LOWER(games.title) = :param", {param: `%LOWER(${param})%`})
+      .where("LOWER(games.title) LIKE :param", {param: `%LOWER(${param})%`})
       .getMany()
       // Complete usando query builder
   }
